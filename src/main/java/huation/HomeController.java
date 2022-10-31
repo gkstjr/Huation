0e97e52ae7839842e06730346e4a2812f7bd74c5
gkstjr <gkstjr8332@naver.com>
2022-10-31 09:53:48 +0900

    upload files

---
+++ src/main/java/huation/HomeController.java
@@ -0,0 +1,14 @@
+package huation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+		public String main() {
+			return "index";
+		}
+
+}
