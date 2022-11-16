baa8aab4b5d032ca09e24d1a5b988488828984b7
gkstjr <gkstjr8332@naver.com>
2022-11-16 18:07:09 +0900

    관리자 게시판 만드는 중

--- huation/src/main/java/huation/home/admin/AdminController.java
+++ huation/src/main/java/huation/home/admin/AdminController.java
@@ -2,8 +2,13 @@ package huation.home.admin;
 
 
 
+import java.util.List;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -16,6 +21,16 @@ public class AdminController {
 	@Autowired
 	private AdminService service;
 	
+//	관리자페이지 들어가기
+	@GetMapping("/admin")
+	public String admin(Model model) {
+		
+		List<AdminDto> list = service.list(); 
+		System.out.println(list);
+		model.addAttribute("boardList",list);
+		return "admin/list";			
+	}
+	
 	@PostMapping("/mailSend")
 	public String insert(@ModelAttribute AdminDto admin) {
 		
@@ -24,7 +39,7 @@ public class AdminController {
 		log.info("hanseok1={}",admin);
 //		service.insert(admin);
 		  AdminDto adminDto= new AdminDto(); 
-		   service.detail(1);
+		   adminDto = service.detail(1);
 		
 		log.info("adminDto = {}" ,adminDto);
 		System.out.println("왜 안되냐고");
