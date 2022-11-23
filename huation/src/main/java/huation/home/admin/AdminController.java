f6b48e7ba90913b93f9340f518432018c1e9df39
gkstjr <gkstjr8332@naver.com>
2022-11-23 10:39:01 +0900

    삭제기능 까지 완료

--- huation/src/main/java/huation/home/admin/AdminController.java
+++ huation/src/main/java/huation/home/admin/AdminController.java
@@ -2,8 +2,11 @@ package huation.home.admin;
 
 
 
+import static org.hamcrest.CoreMatchers.equalTo;
+
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,6 +24,37 @@ public class AdminController {
 	@Autowired
 	private AdminService service;
 	
+	//로그인 화면
+	@GetMapping("/login")
+	public String login() {
+		
+		return "admin/login";
+	}
+	
+	//로그인 체크
+	@PostMapping("/login")
+	public String login(String password,HttpSession session) {
+		
+		System.out.println(password);
+		//로그인 실패시(관리자 비밀번호는 개발자가 정해주는 컨셉?)
+		if(!password.equals("1111")) {
+			return "redirect:/login?error=" + "ok";
+		}
+		
+		session.setAttribute("pw", password);
+		
+		return "redirect:/admin";
+	}
+	
+	//로그아웃
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		
+		session.removeAttribute("pw");
+		
+		return "redirect:/login";
+	}
+	
 	//목록 + 페이지네이션
 	@GetMapping("/admin")
 	public String list(Criteria cri, Model model) {
@@ -35,32 +69,44 @@ public class AdminController {
 		return "admin/list";
 	}
 	
-//	관리자페이지 들어가기
-	@GetMapping("/admin1")
-	public String admin(Model model) {
-		
-		List<AdminDto> list = service.list(); 
-		System.out.println(list);
-		model.addAttribute("boardList",list);
-		return "admin/list2";			
-	}
-	
+	//문의하기 눌렀을 때(contact에서)
 	@PostMapping("/mailSend")
 	public String insert(@ModelAttribute AdminDto admin) {
-		
-		try {
 			
 		log.info("hanseok1={}",admin);
-//		service.insert(admin);
-		  AdminDto adminDto= new AdminDto(); 
-		   adminDto = service.detail(1);
-		
-		log.info("adminDto = {}" ,adminDto);
-		System.out.println("왜 안되냐고");
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-		return "contact/contact";
+			service.insert(admin);
+		  
+		return "redirect:/contact/contact";
+	
 	}
+	
+	//문의하기 눌렀을 때(recruit에서)
+		@PostMapping("/mailSend2")
+		public String insert2(@ModelAttribute AdminDto admin) {
+				
+			log.info("hanseok1={}",admin);
+				service.insert(admin);
+			  
+			return "redirect:/recruit/recruit";
 		
+		}
+		
+	//상세보기
+		@GetMapping("/admin/detail") 
+		public String detail(int boardId,Model model) {
+			
+		AdminDto admin = service.detail(boardId);
+		model.addAttribute("admin",admin);
+	
+			return "admin/detail";
+		}
+		
+		//삭제
+		@GetMapping("/admin/delete") 
+		public String delete(int boardId) {
+			
+			service.delete(boardId);
+			
+			return "redirect:/admin";
+		}
 }
